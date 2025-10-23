181daaac23531e628619bd2d09109ecd9722fb96
MithunKumarRajak <mithunkumarrajak01012005@gmail.com>
2025-10-24 00:09:42 +0530

    Initial commit with Python and C DSA folders

---
+++ DSA WITH PYTHON/MIN_MAX_Value.py
@@ -0,0 +1,6 @@
+arr = [7, 4, 54, 9, 15, 2, 14, 6, 5]
+# print(min(arr))
+print("Lowest value in the array is:", min(arr))
+print("Index of lowest value in the array is:", arr.index(min(arr)))
+print("Highest value in the array is:", max(arr))
+print("Index of highest value in the array is:", arr.index(max(arr)))
---
+++ DSA WITH PYTHON/bubble_sorting.py
@@ -0,0 +1,38 @@
+# Bubble Sort Algorithm in Python
+arr = [12, 34, 23, 45, 67, 89, 90, 11, 9, 5]
+# reverse False means accending order follow karna hai and reverse True mean decending order follow karna hai
+# key = none , means abhi koi key value ya specific nuber ko find anhi kar rahe hai ,abhi apne adjcent number ko compare kar rahe hai
+# accending order ke liye a>b and reverse = false
+# decending order ke liye a<b and reverse = true
+
+n = len(arr)
+
+
+def bubble_sort(arr, reverse=False):
+    swapped = False
+    swap_count = 0
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+           # Yaha pe [j] wala part mai j current index represent kar raha hai
+            a = arr[j]  # It's equivalent to arr[j+0] --> 0 means current index
+            # Yaha pe [j+1] wala part mai j toh index represent kar raha hai aur +1 uske next index ko represent kar raha hai
+            b = arr[j + 1]
+
+            if (a > b and not reverse) or (a < b and reverse):
+                # Yaha pe left side mai current and next element ki value assign hai or right side only order change karne se value swap ho gayi...
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] # Swaping without temp variable
+                ''' OR Other Method to swap using temp variable
+                temp = arr[j]          # store current element in temp
+                arr[j] = arr[j + 1]    # replace current element with next element
+                arr[j + 1] = temp      # assign temp to next element
+                '''
+                swap_count += 1
+                swapped = True
+        if not swapped:
+            break
+    return (arr, swap_count)
+
+# if a function returns multiple values separated by commas, it always returns a single tuple containing those values.
+arr, swap_count = bubble_sort(arr)  # unpacking the returned tuple into two variables
+print("Sorted Array:", arr)
+print("Total swaps:", swap_count)
